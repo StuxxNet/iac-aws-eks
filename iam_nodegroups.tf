bea2de6b156e2ebd1fd23cb50a1a026d2a3ce1df
ramon.silveira-borges@telekom.com <ramon.silveira-borges@telekom.com>
2023-09-28 23:51:34 +0200

    fix: EBS driver

--- iam_nodegroups.tf
+++ iam_nodegroups.tf
@@ -12,9 +12,39 @@ data "aws_iam_policy_document" "nodegroups_assume_role" {
   }
 }
 
+resource "aws_iam_policy" "ebs_csi_driver_policy" {
+  name        = "eks-csi-driver-policy"
+  description = "Policy required to enable K8S to spin it's load-balancers"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [{
+      "Sid" : "VisualEditor0",
+      "Effect" : "Allow",
+      "Action" : [
+        "ec2:CreateVolume",
+        "ec2:DeleteVolume",
+        "ec2:DetachVolume",
+        "ec2:AttachVolume",
+        "ec2:DescribeInstances",
+        "ec2:CreateTags",
+        "ec2:DeleteTags",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes"
+      ],
+      "Resource" : "*"
+    }]
+  })
+}
+
 resource "aws_iam_role" "nodegroups_role" {
   name               = format("%s-nodegroups-role", var.name)
   assume_role_policy = data.aws_iam_policy_document.nodegroups_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy_attachment" {
+  policy_arn = aws_iam_policy.ebs_csi_driver_policy.arn
+  role       = aws_iam_role.nodegroups_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "nodegroups_role_attachment_AmazonEKSWorkerNodePolicy" {
